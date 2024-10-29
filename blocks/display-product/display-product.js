ee9eeacb4940f0e7cb5a520e9bb7de9c40bd34f7
deck-adobe <deckreyes@adobe.com>
2024-10-29 14:09:14 -0700

    debugging

--- blocks/display-product/display-product.js
+++ blocks/display-product/display-product.js
@@ -6,5 +6,19 @@ console.log(urlParams.getAll('related-items'))
 
 
   fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=products&related-items=L1&L2')
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  .then(response => response.json())
+  .then(data => {
+    
+    const tradeDate = data['Global Quote']['07. latest trading day'];
+   
+    // Display title
+    document.getElementById('result-here').textContent = 'Stock Price Information (dynamically generated): ' ;
+
+   
+    // Display the last trade date
+    //document.getElementById('trade-date').textContent = 'Trade Date: ' + tradeDate;
+
+  })
+  .catch(error => {
+    console.log('Error fetching stock price:', error);
+  });
