5889fdf622d564282d28f5eb6546a44d7f083e40
deck-adobe <deckreyes@adobe.com>
2024-10-29 14:58:52 -0700

    debugging

--- blocks/display-product/display-product.js
+++ blocks/display-product/display-product.js
@@ -12,15 +12,18 @@ console.log(urlParams.getAll('related-items'))
     console.log(response);
 
     //const product_name = response.data[0].name;
-    const product_name = response.data.related-items[L1, L2];
+    //const product_name = response.data.related-items[L1, L2];
    
     // Display title
     document.getElementById('result-here').textContent = 'Results' ;
 
+    var slag = JSON.parse(response).filter(doc => doc.related-items === 'test');
+    console.log(slag);
+
 
 
     //Display the last trade date
-    document.getElementById('product-name').textContent = product_name;
+    //document.getElementById('product-name').textContent = product_name;
 
   })
   .catch(error => {
