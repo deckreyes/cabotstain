985c0c9be618ce5cafa1a2cffdbc3f152dae24f1
deck-adobe <deckreyes@adobe.com>
2024-10-29 16:46:20 -0700

    debugging

--- blocks/display-product/display-product.js
+++ blocks/display-product/display-product.js
@@ -24,7 +24,7 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=products')
     //document.getElementById('product-name').textContent = product_name;
 
     for (var i=0; i < response.total; i++){
-        if (response.data[i].related === related_items){
+        if (response.data[i].related === x){
             document.write(response.data[i].name + " - " + response.data[i].related + "<br /><br />")
         }
     }
