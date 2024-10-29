9f0ee90da841fa42b26bc11ccd9f87dbcb681ced
deck-adobe <deckreyes@adobe.com>
2024-10-29 16:37:48 -0700

    debugging

--- blocks/display-product/display-product.js
+++ blocks/display-product/display-product.js
@@ -5,7 +5,7 @@ console.log(urlParams.getAll('related-items'))
 
 const related_items = urlParams.getAll('related-items');
 
-fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=products&related-items=L1&L2')
+fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=products')
   .then(response => response.json())
   .then(response => {
     
