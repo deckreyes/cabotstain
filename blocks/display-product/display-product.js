ca4253176ada7d26993f980aa6de25f0bfb756ec
deck-adobe <deckreyes@adobe.com>
2024-10-29 14:34:06 -0700

    debugging

--- blocks/display-product/display-product.js
+++ blocks/display-product/display-product.js
@@ -11,7 +11,7 @@ console.log(urlParams.getAll('related-items'))
     
     console.log(data);
 
-    const product_name = response.data[0].name;
+    const product_name = response.data.name[0];
    
     // Display title
     document.getElementById('result-here').textContent = 'Results' ;
