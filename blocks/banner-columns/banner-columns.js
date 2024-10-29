60f233c289230aa5a48649f500c8bd11f18cadc5
deck-adobe <deckreyes@adobe.com>
2024-10-29 08:56:40 -0700

    initial commit

---
+++ blocks/banner-columns/banner-columns.js
@@ -0,0 +1,28 @@
+import { initQueryColumns } from './banner-columns-query.js';
+
+export default function decorate(block) {
+  block.style.display = 'none';
+  const isQueryBlock = block.classList.contains('query');
+
+  if (isQueryBlock) {
+    initQueryColumns(block);
+  } else {
+    const cols = [...block.firstElementChild.children];
+    block.classList.add(`columns-${cols.length}-cols`);
+
+    // setup image columns
+    [...block.children].forEach((row) => {
+      [...row.children].forEach((col) => {
+        const pic = col.querySelector('picture');
+        if (pic) {
+          const picWrapper = pic.closest('div');
+          if (picWrapper && picWrapper.children.length === 1) {
+            // picture is only content in column
+            picWrapper.classList.add('banner-columns-img-col');
+          }
+        }
+      });
+    });
+    block.style.display = 'block';
+  }
+}
