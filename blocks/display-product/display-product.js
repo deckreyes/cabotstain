bd43ef946936ce65366153a1ea63daa150fdc5b7
deck-adobe <deckreyes@adobe.com>
2024-10-30 09:30:21 -0700

    debugging

--- blocks/display-product/display-product.js
+++ blocks/display-product/display-product.js
@@ -26,7 +26,7 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=products-new
     for (var i=0; i < response.total; i++){
         //if (response.data[i].related === x){
             //document.write(response.data[i].name + "<br /><br />")
-            document.getElementById('product-name').textContent = response.data[i].name + "<br /><br />"
+            document.getElementById('product-name').textContent = response.data[i].name
         //}
     }
     
