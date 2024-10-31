c7e405b47fcf8a5198116a5be9722593c0498142
deck-adobe <deckreyes@adobe.com>
2024-10-31 16:19:19 -0700

    debugging

--- blocks/display-wood/display-wood.js
+++ blocks/display-wood/display-wood.js
@@ -6,9 +6,9 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=wood')
     console.log(response);
 
  
-           document.getElementById('woodimage1').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[0].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+           document.getElementById('woodimage1').textContent += "<img loading=\"eager\"  src=\"" + response.data[0].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
            document.getElementById('woodtext1').textContent  = response.data[0].name
-           document.getElementById('woodimage2').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[1].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+           document.getElementById('woodimage2').textContent += "<img loading=\"eager\"  src=\"" + response.data[1].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
            document.getElementById('woodtext2').textContent  = response.data[1].name
    
     
