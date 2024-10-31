54c74b7e78f1d0d95f70feb98cd4b3a991a7120a
deck-adobe <deckreyes@adobe.com>
2024-10-31 16:58:05 -0700

    debugging

--- blocks/display-wood/display-wood.js
+++ blocks/display-wood/display-wood.js
@@ -8,7 +8,15 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=wood')
     document.getElementById('wood1').innerHTML  = response.data[0].name
     document.getElementById('wood1').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[0].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
     document.getElementById('wood2').innerHTML  = response.data[1].name
-    document.getElementById('wood3').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[1].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+    document.getElementById('wood2').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[1].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+    document.getElementById('wood3').innerHTML  = response.data[2].name
+    document.getElementById('wood3').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[2].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+    document.getElementById('wood4').innerHTML  = response.data[3].name
+    document.getElementById('wood4').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[3].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+    document.getElementById('wood5').innerHTML  = response.data[4].name
+    document.getElementById('wood5').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[4].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+    document.getElementById('wood6').innerHTML  = response.data[5].name
+    document.getElementById('wood6').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[5].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
        
     
           
