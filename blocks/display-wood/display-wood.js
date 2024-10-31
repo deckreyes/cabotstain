0f22eb3921f2875967527c0706e3d896a5717a7c
deck-adobe <deckreyes@adobe.com>
2024-10-31 15:52:40 -0700

    debugging

--- blocks/display-wood/display-wood.js
+++ blocks/display-wood/display-wood.js
@@ -11,7 +11,7 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=wood')
            document.getElementById(woodimage1).innerHTML = "<img loading=\"eager\"  src=\"" + response.data[0].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
            document.getElementById(woodtext1).innerHTML = response.data[0].name
           
-
+  }
   )
   .catch(error => {
     console.log('Error fetching products', error);
