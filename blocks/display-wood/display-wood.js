0ca468174ebce20f0d72d20bae75e929e359d090
deck-adobe <deckreyes@adobe.com>
2024-10-31 16:56:37 -0700

    debugging

--- blocks/display-wood/display-wood.js
+++ blocks/display-wood/display-wood.js
@@ -5,8 +5,10 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=wood')
     
     console.log(response);
 
-    document.getElementById('woodtext1').innerHTML  = response.data[0].name
-    document.getElementById('woodtext1').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[0].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+    document.getElementById('wood1').innerHTML  = response.data[0].name
+    document.getElementById('wood1').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[0].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+    document.getElementById('wood2').innerHTML  = response.data[1].name
+    document.getElementById('wood3').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[1].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
        
     
           
