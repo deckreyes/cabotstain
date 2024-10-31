8bd3e83740f561b4d569533ffd6a90db7f5faf00
deck-adobe <deckreyes@adobe.com>
2024-10-31 16:12:37 -0700

    debugging

--- blocks/display-wood/display-wood.js
+++ blocks/display-wood/display-wood.js
@@ -8,9 +8,9 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=wood')
     //const product_name = response.data[0].name;
   
 
-           document.getElementById(woodtext1).textContent  = response.data[0].name
+           document.getElementById('woodtext1').textContent  = response.data[0].name
 
-    console.log('hello world first');
+   
 
           
   }
@@ -18,6 +18,3 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=wood')
   .catch(error => {
     console.log('Error fetching products', error);
   });
-
-
-  console.log('hello world second')
