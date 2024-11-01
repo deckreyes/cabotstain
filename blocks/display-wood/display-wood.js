91f20d3a5dd92bca9f2419e2278be3b1161cf224
deck-adobe <deckreyes@adobe.com>
2024-10-31 17:06:25 -0700

    debugging

--- blocks/display-wood/display-wood.js
+++ blocks/display-wood/display-wood.js
@@ -1,3 +1,4 @@
+document.getElementById('select').innerHTML  = "<select name=\"woods\" id=\"woods\" multiple>"
 
 fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=wood')
   .then(response => response.json())
@@ -5,10 +6,10 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=wood')
     
     console.log(response);
 
-    document.getElementById('wood1').innerHTML  = response.data[0].name
-    document.getElementById('wood1').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[0].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
-    document.getElementById('wood2').innerHTML  = response.data[1].name
-    document.getElementById('wood2').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[1].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+    document.getElementById('wood1').innerHTML  = "<option value=\"wood1\">" + response.data[0].name
+    document.getElementById('wood1').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[0].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\"></option>"
+    document.getElementById('wood2').innerHTML  = "<option value=\"wood2\">" +response.data[1].name
+    document.getElementById('wood2').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[1].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\"></option>"
     document.getElementById('wood3').innerHTML  = response.data[2].name
     document.getElementById('wood3').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[2].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
     document.getElementById('wood4').innerHTML  = response.data[3].name
@@ -16,10 +17,8 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=wood')
     document.getElementById('wood5').innerHTML  = response.data[4].name
     document.getElementById('wood5').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[4].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
     document.getElementById('wood6').innerHTML  = response.data[5].name
-    document.getElementById('wood6').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[5].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
-       
     
-          
+              
   }
   )
   .catch(error => {
