66f7b003862e0bc9b90bdaa30d4c0f534a71a193
deck-adobe <deckreyes@adobe.com>
2024-11-04 13:33:42 -0800

    debugging

--- blocks/display-product/display-product.js
+++ blocks/display-product/display-product.js
@@ -22,12 +22,18 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=products-mat
     //document.getElementById('product-name').textContent = product_name;
 
     for (var i=0; i < response.total; i++){
-    if (response.data[i].look == 'clear'){          
+      if (response.data[i].look == 'clear'){          
+          document.getElementById('product-name').innerHTML += "<br />"
+          document.getElementById('product-name').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[i].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+          document.getElementById('product-name').innerHTML += response.data[i].name
+          document.getElementById('product-name').innerHTML += "<br />"
+      } 
+      if (response.data[i].look == 'woodTone'){          
         document.getElementById('product-name').innerHTML += "<br />"
         document.getElementById('product-name').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[i].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
         document.getElementById('product-name').innerHTML += response.data[i].name
         document.getElementById('product-name').innerHTML += "<br />"
-    } 
+      } 
    
    
    }  
