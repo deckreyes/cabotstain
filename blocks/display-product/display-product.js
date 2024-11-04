364b8808fd66eb761d69c3d2a48bf19421838230
deck-adobe <deckreyes@adobe.com>
2024-11-04 13:32:32 -0800

    debugging

--- blocks/display-product/display-product.js
+++ blocks/display-product/display-product.js
@@ -21,17 +21,18 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=products-mat
     //Display the last trade date
     //document.getElementById('product-name').textContent = product_name;
 
-   
-    if (response.data[i].look == 'clear'){        
-      for (var i=0; i < response.total; i++){  
+    for (var i=0; i < response.total; i++){
+    if (response.data[i].look == 'clear'){          
         document.getElementById('product-name').innerHTML += "<br />"
         document.getElementById('product-name').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[i].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
         document.getElementById('product-name').innerHTML += response.data[i].name
         document.getElementById('product-name').innerHTML += "<br />"
-        }
-      } 
-    }
-  )
+    } 
+   
+   
+   }  
+
+  })
   .catch(error => {
     console.log('Error fetching products', error);
   });
