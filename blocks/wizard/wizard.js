42b9b7a9639267dd921e51c7063aa7c9c8eed62b
deck-adobe <deckreyes@adobe.com>
2024-11-04 14:05:08 -0800

    debugging

--- blocks/wizard/wizard.js
+++ blocks/wizard/wizard.js
@@ -32,7 +32,7 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=master')
     if (step === 'four'){
       document.getElementById('header-here').textContent = '4. What is the look you want?' ;
       }
-    if (step === 'fiver'){
+    if (step === 'five'){
       document.getElementById('header-here').textContent = '5. Results' ;
       }
 
@@ -61,7 +61,7 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=master')
       if (response.data[i].brand === brand && response.data[i].type === type && step === 'four'){          
         document.getElementById('surface-type-here').innerHTML += "<br />"
         document.getElementById('surface-type-here').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[i].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
-        document.getElementById('surface-type-here').innerHTML += "<a href=\"wizard?type=product&look=" + response.data[i].id + "&condition=" + condition + "&project=" + project + "&step=five\">" + response.data[i].name + "</a>"
+        document.getElementById('surface-type-here').innerHTML += "<a href=\"wizard?brand=cabot&type=product&look=" + response.data[i].id + "&condition=" + condition + "&project=" + project + "&step=five\">" + response.data[i].name + "</a>"
         document.getElementById('surface-type-here').innerHTML += "<br />"
       } 
       if (response.data[i].brand === brand && response.data[i].type === type && look === look && step === 'five'){          
