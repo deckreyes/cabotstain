b5dc5da380eca9cd57d6a7f01c6cbb4a2a5b6cd2
deck-adobe <deckreyes@adobe.com>
2024-11-04 11:57:47 -0800

    debugging

--- blocks/wizard/wizard.js
+++ blocks/wizard/wizard.js
@@ -52,7 +52,7 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=master')
       if (response.data[i].brand === brand && response.data[i].type === type && step === 'three'){          
         document.getElementById('surface-type-here').innerHTML += "<br />"
         document.getElementById('surface-type-here').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[i].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
-        document.getElementById('surface-type-here').innerHTML += "<a href=\"wizard?brand=cabot&type=look" + look + "&project=" + project + "&condition=" + response.data[i].id + "&step=four\">" + response.data[i].name + "</a>"
+        document.getElementById('surface-type-here').innerHTML += "<a href=\"wizard?brand=cabot&type=look&look=" + look + "&project=" + project + "&condition=" + response.data[i].id + "&step=four\">" + response.data[i].name + "</a>"
         document.getElementById('surface-type-here').innerHTML += "<br />"
       } 
       if (response.data[i].brand === brand && response.data[i].type === type && step === 'four'){          
