5359c50199602dd0f5dd936aff960b42b961ce83
deck-adobe <deckreyes@adobe.com>
2024-11-04 10:41:05 -0800

    debugging

--- blocks/wizard/wizard.js
+++ blocks/wizard/wizard.js
@@ -22,6 +22,10 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=master')
     document.getElementById('header-here').textContent = '2. What does your project look like?' ;
     }
 
+    if (step === 'three'){
+      document.getElementById('header-here').textContent = '3. My wood has?' ;
+      }
+
     //Display the last trade date
     //document.getElementById('product-name').textContent = product_name;
 
@@ -35,10 +39,16 @@ fetch('https://main--cabotstain--deckreyes.aem.page/deck.json?sheet=master')
       if (response.data[i].brand === brand && response.data[i].type === type && step === 'two'){          
         document.getElementById('surface-type-here').innerHTML += "<br />"
         document.getElementById('surface-type-here').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[i].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
-        document.getElementById('surface-type-here').innerHTML += "<a href=\"wizard?brand=cabot&type=condition&id=" + response.data[i].id + "&step=two\">" + response.data[i].name + "xxx</a>"
+        document.getElementById('surface-type-here').innerHTML += "<a href=\"wizard?brand=cabot&type=issues&id=" + response.data[i].id + "&step=three\">" + response.data[i].name + "xxx</a>"
         document.getElementById('surface-type-here').innerHTML += "<br />"
-    } 
-     }  
+      } 
+      if (response.data[i].brand === brand && response.data[i].type === type && step === 'three'){          
+        document.getElementById('surface-type-here').innerHTML += "<br />"
+        document.getElementById('surface-type-here').innerHTML += "<img loading=\"eager\"  src=\"" + response.data[i].image + "?width=200&amp;format=jpg&amp;optimize=medium\" width=\"200\" height=\"200\">"
+        document.getElementById('surface-type-here').innerHTML += "<a href=\"wizard?brand=cabot&type=look&id=" + response.data[i].id + "&step=four\">" + response.data[i].name + "xxx</a>"
+        document.getElementById('surface-type-here').innerHTML += "<br />"
+      } 
+    }  
 
   })
   .catch(error => {
